537e5528e727650c4ea0c1ea919cc95977c85fa3
GustavoSilvaCorrea <gusta0581476@gmai.com>
2025-08-30 11:22:57 -0300

    Arquivo csv

---
+++ Aula03/equipe.c
@@ -0,0 +1,40 @@
+// Implementar todas as funções do TAD Equipe
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "equipe.h"
+
+Time *LerDados(char *Arquivo)
+{
+
+    Time *X = malloc(20 * sizeof(Time)); // malloc (memory allocation) é uma função aloca dinamicamente um bloco de um número específico de bytes na memória (heap)
+    if (X == NULL)
+    {
+        printf("ERRO: memoria insuficiente \n");
+        exit(1);
+    }
+
+    FILE *fp = fopen(Arquivo, "r");
+    if (fp == NULL)
+    {
+        printf("ERRO: Arquivo nao pode ser aberto.");
+        exit(1);
+    }
+
+    int i = 0;
+    char cabecalho[100];
+    fscanf(fp, "%s", cabecalho);
+    while (fscanf(fp,
+                  "%d; %[^;]; %[^;]; %d; %d; %d; %d; %d; %d; %d; %d\n",
+                  &X[i].posicao, X[i].estado, X[i].equipe, &X[i].pts,
+                  &X[i].j, &X[i].v, &X[i].e, &X[i].d, &X[i].gp, &X[i].gc, &X[i].sg) == 11)
+    {
+        X[i].aproveitamento = 100 * (float) X[i].pts / (3 * X[i].j );
+        i++;
+    }
+
+    fclose(fp);
+    return X;
+}
---
+++ Aula03/equipe.h
@@ -0,0 +1,22 @@
+#ifndef EQUIPE_H
+#define EQUIPE_H
+
+typedef struct 
+{
+    int posicao;
+    char estado[30];
+    char equipe[30]; // Nome do Time
+    int pts; // Pontos = Vitoria * 3 + Empate
+    int j; // Jogos
+    int v; // Vitoria
+    int e; // Empate
+    int d; // Derrota
+    int gp; // Gols Pró
+    int gc; // Gols sofridos
+    int sg; // Saldo de Gols = Gols Pró - Gols sofridos
+    float aproveitamento;
+} Time;
+
+Time* LerDados(char* Arquivo);
+
+#endif
